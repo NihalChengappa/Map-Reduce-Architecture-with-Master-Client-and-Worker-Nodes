78691679724718e5402bf06ad1f61f4de8d44973
NihalChengappa <nihalchengappa2625@gmail.com>
2022-11-30 19:09:30 +0530

    10th commit

--- client.py
+++ client.py
@@ -26,6 +26,7 @@ def partition_data(path,no_workers):
 
 
 def client_program():
+    x,y,z=0,0,0
     host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
     port_w=[22234,22235,22236]
@@ -69,12 +70,18 @@ def client_program():
             f_name=input("Input filename:")
             client_socket.send((pickle.dumps(message)))
             data = pickle.loads(client_socket.recv(1024))  # receive response
-            worker_conn1=socket.socket()
-            worker_conn1.connect((host,port_w[0]))
-            worker_conn2=socket.socket()
-            worker_conn2.connect((host,port_w[1]))
-            worker_conn3=socket.socket()
-            worker_conn3.connect((host,port_w[2]))
+            if(x==0):
+                worker_conn1=socket.socket()
+                worker_conn1.connect((host,port_w[0]))
+                x=1
+            if(y==0):
+                worker_conn2=socket.socket()
+                worker_conn2.connect((host,port_w[1]))
+                y=1
+            if(z==0):
+                worker_conn3=socket.socket()
+                worker_conn3.connect((host,port_w[2]))
+                z=1
             worker_conn1.send(pickle.dumps((f_name,"2")))
             worker_conn2.send(pickle.dumps((f_name,"2")))
             worker_conn3.send(pickle.dumps((f_name,"2")))
@@ -84,6 +91,9 @@ def client_program():
             print(f2)
             f3=pickle.loads(worker_conn3.recv(1024))
             print(f3)
+            # worker_conn1.close()
+            # worker_conn2.close()
+            # worker_conn3.close()
         if message=="3":
             mp=input("Enter mapper path:")
             rd=input("Enter reducer path:")
